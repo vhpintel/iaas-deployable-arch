781ee6676e4889f2e6431265d46a79a0ba32f1cb
Vijay Kumar HP <98866009+vhpintel@users.noreply.github.com>
2025-04-10 20:24:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,11 @@
-variable "ibmcloud_api_key" {
-  description = "IBM Cloud API Key"
+variable "ssh_key" {
+  description = "SSH key name"
   type        = string
   default     = ""
 }
+
 variable "ibmcloud_region" {
   description = "IBM Cloud Region"
   type        = string
-  default     = "us-east"
+  default     = ""
 }
-variable "vpc_name" {
-  description = "VPC name"
-  type        = string
-  default     = "test-vpc"
-}
-
